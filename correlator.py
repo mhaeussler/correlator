19f1f6f4c839e2402534425f42e2668cc477fdbf
mhaeussler <haeussler.moritz@gmail.com>
2017-08-27 01:27:09 +0200

    moved everything to separate classes.

--- correlator.py
+++ correlator.py
@@ -12,130 +12,230 @@ import numpy as np
 import matplotlib.pyplot as plt
 import string
 import os
+from collections import deque
 
 ri = np.random.randint
 rf = np.random.random
 
 
 #################################################
+# Classes
+#################################################
+class Correlator:
+    
+    def __init__(self):
+        self.outfilename = "default"
+        self.featureNames = []
+        self.dataset = False
+        self.commandChain = deque()
+
+    
+    def makeFeatureNames(self, n):
+        """returns feature list with unique dummy names according to m."""
+        
+        alphabet = tuple(string.ascii_uppercase)
+        
+        counter = 0;
+        counter1 = 0;
+        for i in range(n):
+            staged = alphabet[i % 26]   # A...Z
+            
+            if( counter % 26 == 0):
+                counter1 += 1
+                
+            if(staged in self.featureNames):
+                self.featureNames.append(staged*counter1)
+            else:
+                self.featureNames.append(staged)
+            
+            counter += 1
+           
+    
+    
+    def makeRawData(self, m, n):
+        """Generates initial data.
+            
+            m - sample size
+            n - number of features
+
+        """
+        self.makeFeatureNames(n)
+        dataset = {}
+        
+        for name in self.featureNames:
+            # -10.0 ... +10.0
+            dataset[name] = np.asarray([10 * rf() for i in range(m)])
+            
+        self.dataset = pd.DataFrame.from_dict(dataset)
+
+
+
+    def correlate(self, dep, ind, command):
+
+        # features to correlate must exist in raw dataset
+        if dep and ind in self.dataset.columns.values:
+
+            # print("dependent:\n", self.dataset[dep].head())
+            # print("independent:\n", self.dataset[ind].head())
+            ind = self.dataset[ind]
+
+            print(command.getInfo())
+            # # set function
+            f = 0
+            if command.ftype == "lin" :
+                f = ind + command.shift
+            elif command.ftype == "sin":
+                f = np.sin(ind + shift)
+            elif command.ftype == "exp":
+                f = np.exp(ind + shift)
+            elif command.ftype == "cos":
+                f = np.cos(ind + shift)
+            elif command.ftype == "log10":
+                f = np.log10(ind + shift)
+            elif command.ftype == "log2":
+                f = np.log2(ind + shift)
+            elif command.ftype == "pow":
+                f = (ind + shift)**command.power
+
+            # set rands
+            rands = 0
+            
+            if command.noise == "normal":
+                rands = np.random.normal(command.mu, command.sig,len(ind))
+            elif noise == "uniform":
+                rands = np.random.uniform(command.uniMin, command.uniMax,len(ind))
+            elif noise == "none":
+                rands = 0
+            
+            self.dataset[dep] = command.raiser + command.slope * f + rands  
+            
+        else:
+            print("ERROR: At least one correlation partner is not in featurepool.")
+
+
+
+    def addCommand(self, command):
+        self.commandChain.append(command)
+
+    def addCommmands(self, commands):
+        # TODO 
+        pass
+
+    def clearCommands(self):
+        self.commandChain.clear()
+
+    def showCommands(self):
+        for command in self.commandChain:
+            print("\t", command.info)
+
+    # correlate two features in raw dataset with given command
+    def processOneCorrelation(self, dep, ind, com):
+        # TODO
+        pass
+
+    def processCommandChain():
+        # TODO
+        pass
+
+
+    # Getter and Setter
+    def makeOutfilename(filename):
+        """Makes hardcoded relative filepath out of given requested name"""
+        
+        # outfilepath = "datasets/"
+        outfilepath = "D:\dev\dtfw\Applications\MDSCorrelationDemo\Data/"
+        outfileending = ".csv"
+        outfilename = str(filename)
+        return (outfilepath + outfilename + outfileending)
+
+
+    def getDataset(self):
+        return self.dataset
+
+
+
+#################################################
+class Command:
+    
+    def __init__(self):
+        # default command
+
+        # possible functional dependencies
+        self.ftypes = set(["lin", "exp", "sin", "cos", "log10", "log2", "pow"])
+
+        # basic function type
+        self.ftype = "lin"
+        self.power = 1
+
+        # adjust function
+        self.raiser = 0
+        self.slope = 0
+        self.shift = 0     # offset for polys
+
+        # add noise
+        self.noise = "none"
+        self.mu = 0        # for normal dist
+        self.sig = 1     # for normal dist
+        self.uniMin = -1   # for uniform dist
+        self.uniMax = 1    # for uniform dist
+        
+        # TODO make command id
+        self.setInfo()
+        
+
+    
+    def setCommand(self, raiser, slope, noise, p1, p2, shift=0, ftype="lin", power=1):
+        
+        # y = raiser + slope * f(x + shift)^power + noise(p1, p2)
+
+        ftype = ftype.lower()
+
+        if ftype in self.ftypes:
+            self.ftype = ftype
+        else:
+            print("ERROR: funtion type not known.")
+            return None
+
+        self.power = power
+        self.raiser = raiser
+        self.slope = slope
+        self.shift = shift
+
+        # set noise
+        if noise == "normal":
+            self.noise = noise
+            self.mu = p1
+            self.sig = p2
+        if noise == "uniform":
+            self.noise = noise
+            self.uniMin = p1
+            self.uniMax = p2
+
+        self.setInfo()
+
+
+        
+    # Getter Setter
+    def setInfo(self):
+        self.info = 'y = {0} + {1} * {2}(x + {3})^{4} + {5}({6},{7}) '.format(
+                self.raiser, self.slope, self.ftype, self.shift, self.power, self.noise, self.mu, self.sig)
+        
+
+    def getInfo(self):
+        return self.info
+
+
+
+#################################################
+class CorrChain:
+    pass
+
+
+
+
+#################################################
 # Methods
 #################################################
-def makeFeatureNames(m):
-    """returns feature list with unique dummy names according to m."""
-    
-    alphabet = tuple(string.ascii_uppercase)
-    
-    ans = []
-    counter = 0;
-    counter1 = 0;
-    for i in range(m):
-        staged = alphabet[i % 26]   # A...Z
-        
-        if( counter % 26 == 0):
-            counter1 += 1
-            
-        if(staged in ans):
-            ans.append(staged*counter1)
-        else:
-            ans.append(staged)
-        
-        counter += 1
-       
-    return ans
-
-
-
-def makeCommand(raiser, slope, noise, p1, p2, shift=0, ftype="lin", power=1):
-    """Command builder for convenience.
-    
-    Returns:
-        command - dict.
-    """
-    ftypes = set(["lin", "exp", "sin", "cos", "log10", "log2", "pow"])
-        
-    if ftype not in ftypes:
-        print("ERROR: funtion type not known. Assume linear...")
-        ftype = ""
-    
-    
-    com = {
-
-        "raiser": raiser,
-        "slope": slope,
-        "noise": noise,
-        "shift": shift,     # offset for polys
-        "mu": p1,        # for normal dist
-        "sig": p2,     # for normal dist
-        "uniMin": p1,   # for uniform dist
-        "uniMax": p2,    # for uniform dist
-        
-        "ftype": ftype,
-        "power": power
-        }
-    
-    # (0, 1, "normal", 0, 1, 0, "lin")
-    info = 'y = {0} + {1} * {2}(x + {3})^{4} + {5}({6},{7}) '.format(
-            com["raiser"], com["slope"], com["ftype"], com["shift"], com["power"], com["noise"], p1, p2)
-    return com, info
-
-
-
-def correlate(x, com):
-    """Generates y values for given x array and command.
-    
-    Returns:
-        y - array."""
-        
-    # make function type
-    ftype = com["ftype"].lower()
-    k = com["shift"]
-    f = 0
-    
-    if ftype == "lin":
-        f = x + k
-    elif ftype == "sin":
-        f = np.sin(x + k)
-    elif ftype == "exp":
-        f = np.exp(x + k)
-    elif ftype == "cos":
-        f = np.cos(x + k)
-    elif ftype == "log10":
-        f = np.log10(x + k)
-    elif ftype == "log2":
-        f = np.log2(x + k)
-    elif ftype == "pow":
-        f = (x + k)**com["power"]
-
-    
-    # make random values
-    rands = 0
-    noise = com["noise"].lower()
-    
-    if noise == "normal":
-        rands = np.random.normal(com["mu"],com["sig"],len(x))
-    elif noise == "uniform":
-        rands = np.random.uniform(com["uniMin"],com["uniMax"],len(x))
-    elif noise == "none":
-        rands = 0
-    else:
-        print("ERROR: noise-type unknown.")
-    
-    # assemble command
-    y  = com["raiser"] + com["slope"] * f + rands 
-    return y
-
-
-def makeOutfilename(filename):
-    """Makes hardcoded relative filepath out of given requested name"""
-    
-    # D:\dev\dtfw\Applications\MDSCorrelationDemo\Data
-    # outfilepath = "datasets/"
-    outfilepath = "D:\dev\dtfw\Applications\MDSCorrelationDemo\Data/"
-    outfileending = ".csv"
-    outfilename = str(filename)
-    return (outfilepath + outfilename + outfileending)
-    
-
 
 def vis(x, y, info):
     xlimits = [-2,9]
@@ -144,38 +244,11 @@ def vis(x, y, info):
     plt.xlim(xlimits)
     plt.ylim(ylimits)
     plt.plot(x,y,"bx", label=info)
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.legend()
-    plt.show()   
-    
-    
-
-def makeData(numFeatures, numElements, corrFeat1, corrFeat2, command):
-    """Generates synthetic correlated dataset.
-    
-    Returns:
-        pandas data frame.
-        bool success value.
-    """
-    featurenamepool = makeFeatureNames(numFeatures)
-
-    dataset = {}
-    
-    if corrFeat1 in featurenamepool and corrFeat2 in featurenamepool:
-
-        for name in featurenamepool:
-            dataset[name] = np.asarray([10 * rf() for i in range(numElements)])
-            
-        # correlate
-        dataset[corrFeat1] = correlate(dataset[corrFeat2], command)
-        
-                
-        return pd.DataFrame.from_dict(dataset), True
-            
-    else:
-        print("ERROR: At least one correlation partner is not in featurepool.")
-        return None, False
-    
-
+    plt.show()
+    # plt.savefig('lin_normalposcorr.pdf', format="pdf")
 
 
 
@@ -183,42 +256,64 @@ def makeData(numFeatures, numElements, corrFeat1, corrFeat2, command):
 # Run
 ######################################
 
-# assemble the correlation command
-raiser = 0
-slope = 0.9
-noise = "normal"
-shift = 0
-mu = 0
-sig = 0.5
-uniMin = -1
-uniMax = 1
-ftype = "lin"   # "lin", "exp", "sin", "cos", "log10", "log2", "pow"
-power = 1
+# assemble correlation commands
+# "lin", "exp", "sin", "cos", "log10", "log2", "pow"
+com1 = Command()
+com1.setCommand(0,0.7,"normal",0,1)
 
-com, info = makeCommand(raiser, slope, noise, mu, sig, shift, ftype, power)
+com2 = Command()
+com2.setCommand(0, 0.5, "normal", 0.3, 2)
 
 
-# assemble dataset
-numFeatures = 4
-numElements = 40
-dependent = "A"     # dependent target
-independent = "C"
+#####################################
+# assemble initial raw dataset
+m = 20    # number of rows to generate
+n = 4     # number of features
+# determine which feature pair gets correlated
+# must be in bounds of numFeatures
+# example:  numFeatures = 5 --> [A,...,E]
+# example:  numFeatures = 7 --> [A,...,G]
+correlator = Correlator()
+correlator.makeRawData(m, n)
 
-df, success = makeData(numFeatures, numElements, dependent, independent, com)
+correlator.addCommand(com1)
+correlator.addCommand(com2)
 
 
-if success:
-    # print(df.head())
-    # df.plot("A", "C", kind="scatter")
+
+# data = correlator.getDataset()
+# print(data.head())
+# correlator.correlate("A", "C", com1)
+# print(data.head())
+
+# print(data.columns.values)
+# plt.plot(data)
+# plt.legend()
+# plt.show()
+
+# print("\nCorrelation Table:")
+# print(data.corr())
+
+# print("A-B", pear(data["A"], data["B"]))
+# print(pear(data["A"], data["C"]))
+# print(pear(data["A"], data["D"]))
+# print(pear(data["B"], data["C"]))
+# print(pear(data["B"], data["D"]))
+# print(pear(data["C"], data["D"]))
+
+
+# vis(data.A, data.B, com1.info)
+#    plt.legend()
+#    plt.show()
     
-    outfilename = makeOutfilename("linCorr_[{0}x{1}]_d{2}~{3}~i{4}".format(
-            numElements, numFeatures, dependent, com["ftype"], independent))
-    df.to_csv(outfilename)
-    
-    print("file generated:", outfilename)
-    
-    outFileSize = os.path.getsize(outfilename)
-    print("filesize:", round(outFileSize / 1000000, 2), "MB.")
+# outfilename = makeOutfilename("lin_[{0}x{1}]_d{2}~{3}~i{4}".format(
+#         numElements, numFeatures, dependent, com["ftype"], independent))
+# df.to_csv(outfilename)
+
+# print("file generated:", outfilename)
+
+# outFileSize = os.path.getsize(outfilename)
+# print("filesize:", round(outFileSize / 1000000, 2), "MB.")
     
 
 
